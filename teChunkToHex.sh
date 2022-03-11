78ff4beac7d4a97e0a82e1b5aa0a1a0b302ebb49
Florian Wagner <81021153+florianwagnerdotme@users.noreply.github.com>
2022-03-11 13:04:56 +0100

    Initital commit

---
+++ teChunkToHex.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+#author: Florian Wagner
+#date of creation: 10/03/22
+#
+#############################################
+#Script to calc the size of a payload in HEX#
+#############################################
+
+if [[ -z "$1" ]]; then
+   echo "Please provide a file like this: # ./tePayloadToHex.sh ./Path/To/Txt/With/Payload"
+   exit
+fi
+
+#get the bytecount of the payload file in UNIX form
+charsInFile=$(wc -c $1|cut -d " " -f1)
+
+echo "Chars in file with UNIX CR count: $charsInFile"
+
+#get the lines of the file to calc the CRLF
+linesInFile=$(wc -l $1|cut -d " " -f1)
+
+#original char count with substraction of UNIX CR count
+charsInFile=$(($charsInFile-$linesInFile))
+echo "Chars in file: $charsInFile"
+
+#calc CRLF like it is mentioned in the HTTP RFC
+httpCRLFCount=$((($linesInFile -1) * 2))
+
+echo "HTTP CRCL count: $httpCRLFCount"
+bytecount=$(($charsInFile+$httpCRLFCount))
+printf "\nResult with CRLF counted like in RFC2616\n\n"
+echo "byte count in DEC: "
+echo $bytecount
+
+echo "byte count in HEX:"
+echo $(printf "%x" $bytecount)
+
